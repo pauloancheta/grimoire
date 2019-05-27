a519bda5334de5bcafdc41657ae35ec0fca452ae
Paulo Ancheta <paulo.ancheta@lendesk.com>
2019-05-27 08:56:31 -0700

    New config

---
+++ dotfiles/fzf.zsh
@@ -0,0 +1,16 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_OPTS='--height 60% --layout=reverse'
+export FZF_CTRL_T_OPTS="--preview='bat --color=always {}'"
