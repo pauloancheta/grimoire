2e8f4b120e001cee22e8b8899500c228ebf292a8
Paulo Ancheta <paulo.ancheta@gmail.com>
2022-11-17 08:49:47 -0800

    updates on plugins

--- dotfiles/fzf.zsh
+++ dotfiles/fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 export FZF_DEFAULT_OPTS='--height 60% --layout=reverse'
 export FZF_CTRL_T_OPTS="--preview='bat --color=always {} --theme=gruvbox-dark'"
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
